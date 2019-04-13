647d0866332ae90e5488eb8d2b19640e0886618b
abenetka <ali.benetka@gmail.com>
2019-04-13 12:02:11 -0600

    Adds feature test for landing page

---
+++ spec/features/user_can_see_landing_page_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'as a user when they visit the sit' do
+  it 'shows welcome page' do
+    visit root_path
+
+    expect(page).to have_content("Move Time")
+  end
+end
