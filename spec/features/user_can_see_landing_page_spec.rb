928b36c7b0a6530bd3571509ab06e489176e2679
abenetka <ali.benetka@gmail.com>
2019-04-13 14:32:38 -0600

    Updates feature test for search bar

--- spec/features/user_can_see_landing_page_spec.rb
+++ spec/features/user_can_see_landing_page_spec.rb
@@ -3,7 +3,21 @@ require 'rails_helper'
 describe 'as a user when they visit the sit' do
   it 'shows welcome page' do
     visit root_path
-
+    expect(current_path).to eq(root_path)
     expect(page).to have_content("Move Time")
   end
+
+  it 'can search for a location' do
+    visit root_path
+
+    within '.search_location_container' do
+      expect(page).to have_content("Where do you want to move?")
+      expect(page).to have_button("Search")
+      fill_in :location, with: "Denver, CO"
+      click_on "Search"
+    end
+    expect(current_path).to eq(search_path)
+    expect(page).to have_content("Population: ")
+  end
+
 end
