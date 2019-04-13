0bf4b64c83e811ad5698ae0695c11491fc1f7e0d
abenetka <ali.benetka@gmail.com>
2019-04-13 17:58:09 -0600

    Updates open data service spec for changes

--- spec/services/open_data_service_spec.rb
+++ spec/services/open_data_service_spec.rb
@@ -9,7 +9,7 @@ describe OpenDataService do
   it 'gets population of location' do
     open_data_service = OpenDataService.new
     location = "Denver, CO"
-    population = open_data_service.get_population(location)[:records].first[:fields][:population]
-    expect(population).to eq(2888227)
+    population = open_data_service.get_census_data(location)[:records].first[:fields][:total_population]
+    expect(population).to eq(682545)
   end
 end
