d239e105a480bdf9db20fdbd89a084be828341f7
abenetka <ali.benetka@gmail.com>
2019-04-13 18:01:25 -0600

    Updates service

--- app/services/open_data_service.rb
+++ app/services/open_data_service.rb
@@ -1,14 +1,5 @@
 class OpenDataService
 
-  # def get_population(location)
-  #   response = conn.get("/api/records/1.0/search/?dataset=us-population-urban-area") do |f|
-  #     f.params[:q] = location
-  #     f.params[:sort] = "year"
-  #     f.params[:rows] = 1
-  #   end
-  #   JSON.parse(response.body, symbolize_names: true)
-  # end
-
   def get_census_data(location)
     response = conn.get("/api/records/1.0/search/?dataset=us-cities-demographics") do |f|
       f.params[:q] = location
