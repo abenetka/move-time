0d86b8124fbea71b45a306271ee398c37de4b772
abenetka <ali.benetka@gmail.com>
2019-04-13 20:18:31 -0600

    Adds tests for WalkScore Service

--- spec/services/walk_score_service_spec.rb
+++ spec/services/walk_score_service_spec.rb
@@ -6,10 +6,25 @@ describe WalkScoreService do
     expect(walk_score_service).to be_a(WalkScoreService)
   end
 
-  it 'gets the walk score of a location' do
+  it 'gets the walk score and description of a location' do
     walk_score_service = WalkScoreService.new
-    location = "Denver, CO"
-    # walk_score = walk_score_service.get_census_data(location)[:records].first[:fields][:total_population]
-    # expect(walk_score).to eq(682545)
+    lat = 39.739
+    lon = -104.9903
+
+    walk_score = walk_score_service.get_walk_score(lat, lon)
+    walk_description = walk_score_service.get_walk_score(lat, lon)
+    expect(walk_score[:walkscore]).to eq(93)
+    expect(walk_score[:description]).to eq("Walker's Paradise")
+  end
+
+  it 'gets the bike score and description of a location' do
+    walk_score_service = WalkScoreService.new
+    lat = 39.739
+    lon = -104.9903
+
+    bike_score = walk_score_service.get_walk_score(lat, lon)
+    bike_description = walk_score_service.get_walk_score(lat, lon)
+    expect(bike_score[:bike][:score]).to eq(94)
+    expect(bike_score[:bike][:description]).to eq("Biker's Paradise")
   end
 end
