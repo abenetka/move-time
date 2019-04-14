b2fa60d72e6a177dba98b769b6942c2612c816ba
abenetka <ali.benetka@gmail.com>
2019-04-13 18:01:51 -0600

    Adds find_median_age and find_avg_household_size methods to location facade

--- app/facades/location_facade.rb
+++ app/facades/location_facade.rb
@@ -3,11 +3,22 @@ class LocationFacade
     @location = location
   end
 
-  def find_location
-    location_service[:records].first[:fields][:population]
+  def find_population
+    data = location_service.get_census_data(@location)
+    data[:records].first[:fields][:total_population]
+  end
+
+  def find_median_age
+    data = location_service.get_census_data(@location)
+    data[:records].first[:fields][:median_age]
+  end
+
+  def find_avg_household_size
+    data = location_service.get_census_data(@location)
+    data[:records].first[:fields][:average_household_size]
   end
 
   def location_service
-    OpenDataService.new.get_population(@location)
+    OpenDataService.new
   end
 end
