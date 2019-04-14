2d1ef9e8ceef4dd7fc4ec7e08407d1601b41ea9a
abenetka <ali.benetka@gmail.com>
2019-04-13 18:01:11 -0600

    Refactors get population into a dynamic get_census_data method

--- app/services/open_data_service.rb
+++ app/services/open_data_service.rb
@@ -1,16 +1,24 @@
 class OpenDataService
 
-  def get_population(location)
-    response = conn.get("/api/records/1.0/search/?dataset=us-population-urban-area") do |f|
+  # def get_population(location)
+  #   response = conn.get("/api/records/1.0/search/?dataset=us-population-urban-area") do |f|
+  #     f.params[:q] = location
+  #     f.params[:sort] = "year"
+  #     f.params[:rows] = 1
+  #   end
+  #   JSON.parse(response.body, symbolize_names: true)
+  # end
+
+  def get_census_data(location)
+    response = conn.get("/api/records/1.0/search/?dataset=us-cities-demographics") do |f|
       f.params[:q] = location
-      f.params[:sort] = "year"
       f.params[:rows] = 1
     end
     JSON.parse(response.body, symbolize_names: true)
   end
 
   private
-  
+
   def conn
     Faraday.new("https://public.opendatasoft.com") do |f|
       f.adapter Faraday.default_adapter
