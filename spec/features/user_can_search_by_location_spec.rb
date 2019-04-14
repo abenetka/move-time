59e5e98cc8db3ad8356140ba27407f5e2c686627
abenetka <ali.benetka@gmail.com>
2019-04-13 20:18:46 -0600

    Adds feature tests for viewing results of walkscore service

--- spec/features/user_can_search_by_location_spec.rb
+++ spec/features/user_can_search_by_location_spec.rb
@@ -24,11 +24,18 @@ describe 'as a user when they visit the sit' do
     location = "Denver, CO"
     median_age = 34.1
     avg_household_size = 2.33
-
+    walk_score = 93
+    walk_description = "Walker's Paradise"
+    bike_score = 94
+    bike_description = "Biker's Paradise"
     expect(page).to have_content("#{location}")
     expect(page).to have_content("Population: #{population} people")
     expect(page).to have_content("Median Age: #{median_age} years old")
     expect(page).to have_content("Average Household Size: #{avg_household_size} people")
+    expect(page).to have_content("#{location} is a #{walk_description}")
+    expect(page).to have_content("It has a walkability score of #{walk_score}")
+    expect(page).to have_content("and is a #{bike_description}")
+    expect(page).to have_content(" and a bikeability score of #{bike_score}")
   end
 
 
